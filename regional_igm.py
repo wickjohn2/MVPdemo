de4c887e999b10da914621073ed732079c62face
wickjohn2 <johnwick2@gmail.com>
2017-03-02 10:45:43 -0600

    Update regional_igm.py

--- regional_igm.py
+++ regional_igm.py
@@ -69,7 +69,7 @@ def GenerateConfig(context):
                       'initializeParams': {
                           'sourceImage':
                               URL_BASE +
-                              'debian-cloud/global/images/family/debian-8'
+                              'albatross-keving-sandbox/zones/us-central1-a/disks/web-1'
                       }
                   }]
               }
