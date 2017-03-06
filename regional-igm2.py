b58b149520d3f70e2e78f3f6a6c96d8392eb7165
wickjohn2 <johnwick2@gmail.com>
2017-03-06 17:56:21 -0600

    Update regional-igm2.py

--- regional-igm2.py
+++ regional-igm2.py
@@ -88,7 +88,6 @@ def GenerateConfig(context):
               'baseInstanceName': deployment + '-instance',
               'instanceTemplate': '$(ref.' + instance_template + '.selfLink)',
               'targetSize': 1,
-              'targetPool': '$(ref.' + tp_name + '.selfLink)',
               'autoHealingPolicies': [{
                   'initialDelaySec': 60
               }]
