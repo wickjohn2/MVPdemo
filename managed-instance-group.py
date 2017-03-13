e0b2e9d093d1db4804e323e9cfa09bf448f7807a
wickjohn2 <johnwick2@gmail.com>
2017-03-13 17:03:58 -0500

    Update managed-instance-group.py

--- managed-instance-group.py
+++ managed-instance-group.py
@@ -1,27 +1,33 @@
-# Creates a managed instance group.
+"""Creates a managed instance group."""
 
+# Create Instance Group Manager
 
-# This will be used to build the Web and App Tiers
+URL_BASE = 'https://www.googleapis.com/compute/v1/projects/'
+
+# Every Python Template needs to have the GenerateConfig() or generate_config()
+# method
+# This method is called by DM in expansion and must return either:
+#    - the yaml format required by DM
+#    - a python dictionary representing the yaml (this is more efficient)
 
 def GenerateConfig(context):
-# Generates the configuration
+  """Generates the configuration."""
 
-# Create a dictionary which represents the resources
-
+  # Create a dictionary which represents the resources
+  # (Intstance Template, IGM, etc.)
   resources = [
       {
           'name': context.env['name'],
           'type': 'compute.v1.regionInstanceGroupManager',
           'properties': {
-              'region': context.properties[‘region’],
+              'region': region,
               'baseInstanceName': context.env['name'],
-              'instanceTemplate': 
-                  context.properties['instanceTemplate'],
+              'instanceTemplate': context.properties['instanceTemplate'],
               'targetSize': 1,
               'autoHealingPolicies': [{
                   'initialDelaySec': 60
               }]
           }
-      }
+      },
   ]
-return {'resources': resources}
+  return: {'resources': resources}
