ef13e93e7dac6b29016d9dfea95acc139140ef19
wickjohn2 <johnwick2@gmail.com>
2017-03-13 12:26:31 -0500

    Create autoscaling.py

---
+++ autoscaling.py
@@ -0,0 +1,27 @@
+"""Build Application Tier from Components"""
+
+def GenerateConfig(context):
+  """Generates the configuration."""
+
+  # Create a dictionary which represents the resources
+
+  resources = [
+      {
+          'name': context.env['name'],
+          'type': 'compute.v1.regionAutoscaler',
+          'properties': {
+              'target': context.properties['managedInstanceGroup'],
+              'region': context.properties['region'],
+              'autoscalingPolicy': {
+                  'minNumReplicas': context.properties['minSize'],
+                  'maxNumReplicas': context.properties['maxSize'],
+                  'cpuUtilization': {
+                      'utilizationTarget': 
+                          context.properties['cpuUtil']
+                  },
+                  'coolDownPeriodSec': 90
+              }
+          }
+      }
+  ]
+return {'resources': resources}
